2eae119019656d6064d46390be96e4fdfdf9a071
Eyvazliamil <167743489+Eyvazliamil@users.noreply.github.com>
2026-07-04 12:52:09 +0400

    Update DocumentProgram.cs

--- DocumentSystem/DocumentProgram.cs
+++ DocumentSystem/DocumentProgram.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DocumentSystem
+{
+    public class DocumentProgram
+    {
+        public void OpenDocument() => Console.WriteLine("Document Opened");
+        public virtual void EditDocument() => Console.WriteLine("Can Edit in Pro and Expert versions");
+        public virtual void SaveDocument() => Console.WriteLine("Can Save in Pro and Expert versions");
+    }
+}
